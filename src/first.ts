d26394f81e42582d90e02bad9a39d9375b80314a
Levi Wallace <guysmiley.levi@gmail.com>
2019-09-15 13:06:06 -0700

    Selection Sort

--- src/first.ts
+++ src/first.ts
@@ -1,7 +1,26 @@
-function addOne(x: number) {
-    return x + 1;
+
+function isSorted(xs: number[]): boolean {
+    for (let i = 0; i < (xs.length-1); i++) {
+        if (xs[i] > xs[i+1]) {
+            return false;
+        }
+    }    
+    return true;   
 }
 
+function selectionSort(xs: number[]): number[] {
+    for (let i = 0; i < xs.length-1; i++) {
+        let indx = i;
+        for (let j = i+1; j < xs.length; j++) {
+            if (xs[indx] > xs[j]) {
+                indx = j;
+            }
+        }
+        let b = xs[indx];
+        xs[indx] = xs[i];
+        xs[i] = b;
+    }
+    return xs;
+}
 
-main();
-
+console.log(selectionSort([9, 2, 4, 5, 1, 8, 7, 3]));
