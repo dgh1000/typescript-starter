08c6cb30bc63dc0e894bf1f110d5c1abde4c79d6
Levi Wallace <guysmiley.levi@gmail.com>
2019-09-22 17:32:19 -0700

    homework

--- dist/tsc.js
+++ dist/tsc.js
@@ -1,7 +1,93 @@
-function addOne(x) {
-    return x + 1;
+function isSorted(xs) {
+    for (var i = 0; i < (xs.length - 1); i++) {
+        if (xs[i] > xs[i + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
-main();
+function swap(xs, i1, i2) {
+    var b = xs[i1];
+    xs[i1] = xs[i2];
+    xs[i2] = b;
+}
+function selectionSort(xs) {
+    for (var i = 0; i < xs.length - 1; i++) {
+        var indx = i;
+        for (var j = i + 1; j < xs.length; j++) {
+            if (xs[indx] > xs[j]) {
+                indx = j;
+            }
+        }
+        swap(xs, indx, i);
+    }
+    return xs;
+}
+function bubbleSortPass(xs) {
+    var hasSwapped = false;
+    for (var i = 0; i < xs.length - 1; i++) {
+        if (xs[i] > xs[i + 1]) {
+            swap(xs, i + 1, i);
+            hasSwapped = true;
+        }
+    }
+    return hasSwapped;
+}
+function merge(xs1, xs2) {
+    var out = [];
+    var i = 0;
+    var j = 0;
+    while (i < xs1.length && j < xs2.length) {
+        if (xs1[i] < xs2[j]) {
+            out.push(xs1[i]);
+            if (i < xs1.length)
+                i++;
+        }
+        else if (xs2[j] < xs1[i]) {
+            out.push(xs2[j]);
+            if (j < xs2.length)
+                j++;
+        }
+    }
+    return out;
+}
+function mergeSort(xs) {
+    if (xs.length > 2) {
+        var l1 = [];
+        var l2 = [];
+        for (var i = 0; i < xs.length; i++) {
+            if (i < xs.length / 2)
+                l1.push(xs[i]);
+            else
+                l2.push(xs[i]);
+        }
+        console.log(l1);
+        console.log(l2);
+        console.log("0");
+        return merge(l1, l2);
+    }
+    else if (xs.length == 2) {
+        if (xs[0] > xs[1]) {
+            swap(xs, 0, 1);
+            return xs;
+        }
+    }
+    else if (xs.length == 1) {
+        return xs;
+    }
+    else {
+        return [];
+    }
+}
+function bubbleSort(xs) {
+    while (bubbleSortPass(xs)) {
+    }
+}
+var xs = [9, 2, 4, 5, 1, 8, 7, 3];
+bubbleSort(xs);
+var xs2 = [1, 3, 5, 6];
+var xs1 = [2, 4, 8, 10];
+console.log(mergeSort(xs));
 function bubblePass(xs) {
     throw "err";
 }
--- src/first.ts
+++ src/first.ts
@@ -1,4 +1,3 @@
-
 function isSorted(xs: number[]): boolean {
     for (let i = 0; i < (xs.length-1); i++) {
         if (xs[i] > xs[i+1]) {
@@ -6,6 +5,12 @@ function isSorted(xs: number[]): boolean {
         }
     }    
     return true;   
+}
+
+function swap(xs: number[], i1: number, i2: number): void {
+    let b = xs[i1];
+    xs[i1] = xs[i2];
+    xs[i2] = b;
 }
 
 function selectionSort(xs: number[]): number[] {
@@ -16,11 +21,84 @@ function selectionSort(xs: number[]): number[] {
                 indx = j;
             }
         }
-        let b = xs[indx];
-        xs[indx] = xs[i];
-        xs[i] = b;
+        swap(xs, indx, i);
     }
     return xs;
 }
 
-console.log(selectionSort([9, 2, 4, 5, 1, 8, 7, 3]));
+function bubbleSortPass(xs: number[]): boolean {
+    let hasSwapped = false;
+    for (let i = 0; i < xs.length-1; i++) {
+        if (xs[i] > xs[i+1]) {
+            swap(xs, i+1, i);
+            hasSwapped = true;
+        }
+    }
+    return hasSwapped;
+}
+
+function merge(xs1: number[], xs2: number[]): number[] {
+    let out = []; 
+    let i = 0;
+    let j = 0;
+    while (i < xs1.length && j < xs2.length) {
+        if (xs1[i] < xs2[j]) {
+            out.push(xs1[i]);
+            if (i < xs1.length)
+                i++;
+        }
+        else if (xs2[j] < xs1[i]) {
+            out.push(xs2[j]);
+            if (j < xs2.length)
+                j++;
+        }
+    }
+    return out;
+}
+
+
+function mergeSort(xs: number[]): number[] {
+    if (xs.length > 2) {
+        let l1: number[] = [];
+        let l2: number[] = [];
+        for(let i = 0; i < xs.length; i++) {
+            if (i < xs.length/2)
+                l1.push(xs[i]);
+            else
+                l2.push(xs[i]);
+        }
+        return merge(l1, l2);
+    }
+
+    
+    else if (xs.length == 2)  {
+        if (xs[0] > xs[1]) {
+            swap(xs, 0, 1);
+            return xs;
+        }
+    }
+
+    else if (xs.length == 1) {
+        return xs;
+    }
+
+    else {
+        return [];
+    }
+}
+    
+function bubbleSort(xs: number[]): void {
+    while (bubbleSortPass(xs)) {
+
+    }
+
+}
+
+let xs = [9, 2, 4, 5, 1, 8, 7, 3];
+bubbleSort(xs)
+
+let xs2 = [1, 3, 5, 6]
+let xs1 = [2, 4, 8, 10]
+console.log(mergeSort(xs));
+
+
